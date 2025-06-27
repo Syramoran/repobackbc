deb857da7cef375606efbdf8576e94ba9fd17c3c
Syramoran <syramoranicr@gmail.com>
2025-06-27 18:32:59 -0300

    bbdd

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,13 @@ import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
     UsersModule,
     AppointmentsModule,
     AuthModule,
@@ -28,28 +34,28 @@ import { APP_GUARD } from '@nestjs/core';
     ThrottlerModule.forRoot({
       throttlers: [
         {
-        name: 'short',
-        ttl: 1000,
-        limit: 3,
-      },
-      {
-        name: 'medium',
-        ttl: 10000,
-        limit: 20
-      },
-      {
-        name: 'long',
-        ttl: 60000,
-        limit: 100
-      }
+          name: 'short',
+          ttl: 1000,
+          limit: 3,
+        },
+        {
+          name: 'medium',
+          ttl: 10000,
+          limit: 20
+        },
+        {
+          name: 'long',
+          ttl: 60000,
+          limit: 100
+        }
       ],
     }),],
   controllers: [AppController],
   providers: [AppService,
     {
-  provide: APP_GUARD,
-  useClass: ThrottlerGuard
-}
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
   ],
 })
 export class AppModule { }
